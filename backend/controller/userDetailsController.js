2d8875c00b6df2aac84c37d05f824ea1570fff0b
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-04 22:54:42 +0530

    add post route of user details

--- backend/controller/userDetailsController.js
+++ backend/controller/userDetailsController.js
@@ -9,10 +9,23 @@ export const getUser = async (req, res) => {
   }
 };
 
+// @route: GET /user/details
 export const getUserDetails = async (req, res) => {
   try {
     const userDetails = await UserDetail.find();
     res.status(200).json(userDetails);
+  } catch (error) {
+    res.status(404).json({ errMessage: error });
+  }
+};
+
+// @route: POSt /user/details
+export const postUserDetails = async (req, res) => {
+  const body = req.body;
+  const userDetails = new UserDetail(body);
+  try {
+    const newUserDetails = await userDetails.save();
+    res.status(200).json(newUserDetails);
   } catch (error) {
     res.status(404).json({ errMessage: error });
   }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,12 +1,18 @@
 import express from "express";
 const router = express.Router();
-import { getUser, getUserDetails } from "../controller/userController.js";
+import {
+  getUser,
+  getUserDetails,
+  postUserDetails,
+} from "../controller/userDetailsController.js";
 
 // As a whole route
 router.get("/", getUser);
 
 // Individual routes
+
 // Details routes
 router.get("/details", getUserDetails);
+router.post("/details", postUserDetails);
 
 export default router;
