5020abe362b762da72b0dae2aa91f0cf5b722571
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-17 00:50:02 +0530

    test added for GET all details

--- backend/tests/details.test.js
+++ backend/tests/details.test.js
@@ -1,16 +1,16 @@
 import request from "supertest";
 import app from "../app.js";
+import chai from "chai";
+var assert = chai.assert;
 
-test("GET all details", (done) => {
-  request(app)
+test("GET all details", async (done) => {
+  await request(app)
     .get("/user/details")
     .expect("Content-Type", /json/)
     .expect(200)
     .then((res) => {
+      assert.typeOf(res.body, "Array");
+
       done();
     });
-});
-
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 * 2).toBe(2);
-});
+}, 30000);
