d25b88e1dd2e63804580044ae20b5e087e1ccc59
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-17 22:50:39 +0530

    add test suit for Cannot POST more than 1 document user documents

--- backend/tests/documents.test.js
+++ backend/tests/documents.test.js
@@ -60,3 +60,58 @@ test("GET 1 document", (done) => {
       done();
     });
 });
+
+// Cannot POST document
+test("Cannot POST document more than 1", (done) => {
+  return request(app)
+    .post("/user/documents/post")
+    .send({
+      adhaar_card_no: 1234,
+      driver_license: "142464556",
+      panCard: {
+        pan_card: "helloBro",
+      },
+      voter_auth: "lol",
+      passport: "nanuSingh",
+      password: "ThisIsSecrets2",
+      layer: 10,
+    })
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(404)
+    .then((res) => {
+      expect(res.body).toBeInstanceOf(Object);
+      expect(res.body).toHaveProperty(
+        "message",
+        "one document already be given"
+      );
+      done();
+    });
+});
+
+// UPDATE Document
+test("UPDATE document", (done) => {
+  return request(app)
+    .put("/user/documents")
+    .send({ passport: "bolbo na", adhaar_card_no: 1234 })
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toBeInstanceOf(Object);
+      expect(Object.keys(res.body)).toEqual(
+        expect.arrayContaining([
+          "adhaar_card_no",
+          "driver_license",
+          "panCard",
+          "voter_auth",
+          "passport",
+          "layer",
+          "password",
+        ])
+      );
+      expect(res.body.passport).toEqual("bolbo na");
+      expect(res.body.adhaar_card_no).toEqual(1234);
+      done();
+    });
+});
