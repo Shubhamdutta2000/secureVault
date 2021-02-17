cbc2d2df458cf8519ba454aa6d5fffa9c59e6245
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-18 00:50:26 +0530

    add test suit for POST all finance details

--- backend/tests/finance.test.js
+++ backend/tests/finance.test.js
@@ -64,3 +64,42 @@ test("GET finance details by ID", (done) => {
       done();
     });
 });
+
+// POST all finance details
+test("POST all finance details", (done) => {
+  return request(app)
+    .post("/user/finance")
+    .send({
+      panCard: {
+        panCard: "12345678",
+      },
+      itr_forms: "aye",
+      bank_transaction: "200000",
+      assets: {
+        commodities: "yoHoney",
+        cryptocurrencies: "wolf",
+        stocks: "null",
+        mutual_funds: "nill",
+        ipos: "null",
+      },
+      password: "SECRET2",
+      layer: 9,
+    })
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/)
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toBeInstanceOf(Object);
+      expect(Object.keys(res.body)).toEqual(
+        expect.arrayContaining([
+          "panCard",
+          "itr_forms",
+          "bank_transaction",
+          "assets",
+          "password",
+          "layer",
+        ])
+      );
+      done();
+    });
+});
