5da9ebc3598ba2a11e5b3d363a674aa3ab252240
Shubhamdutta2000 <dondutta2000@gmail.com>
2021-02-27 01:43:26 +0530

    add all secrets section in homepage with proper styling

---
+++ frontend/src/screen/Custom Styles/homeScreen.js
@@ -0,0 +1,72 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles({
+  heading: {
+    position: "absolute",
+    top: "18rem",
+    fontStyle: "italic",
+    fontWeight: "500",
+    fontSize: "4.4rem",
+    lineHeight: "127px",
+    letterSpacing: " -0.001em",
+    background:
+      "linear-gradient(10.56deg,  #003145    0.49%,rgba(0, 152, 176, 0.26) 114.34%)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  },
+
+  para: {
+    position: "absolute",
+    top: "51.6%",
+    fontStyle: "italic",
+    fontWeight: "500",
+    fontSize: "2.2rem",
+    lineHeight: "1rem",
+    letterSpacing: "-0.001em",
+    color: "#1488B5",
+  },
+  image: {
+    width: "32rem",
+    marginLeft: "6rem",
+  },
+  button: {
+    position: "absolute",
+    top: "60%",
+    background:
+      "linear-gradient(90.56deg, #0079A9 0.49%, rgba(100, 200, 240, 0.74) 114.34%)",
+    borderRadius: "28rem",
+    width: "16rem",
+    height: "4.2rem",
+
+    fontStyle: "italic",
+    fontWeight: "bold",
+    fontSize: "1.7rem",
+    textAlign: "center",
+    letterSpacing: "-0.01em",
+    color: "#FFFFFF",
+    letterSpacing: "0.005em",
+    filter: "drop-shadow(12px 12px 10px rgba(0, 0, 0, 0.42))",
+  },
+  allSecrets: {
+    paddingTop: "5.4rem",
+    fontStyle: "italic",
+    fontWeight: "500",
+    fontSize: "5.4rem",
+    lineHeight: "127px",
+    letterSpacing: "-0.01em",
+    color: "#005D81",
+  },
+  doc: {
+    paddingTop: "4rem",
+    filter: "drop-shadow(10px 10px 24px rgba(0, 0, 0, 0.42))",
+  },
+  folder_name: {
+    fontStyle: "italic",
+    fontWeight: "bold",
+    fontWeight: "400",
+    fontSize: "3.2rem",
+    lineHeight: "6rem",
+    letterSpacing: "-0.01em",
+    color: "#016E99",
+  },
+});
--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,80 +1,131 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import { Button } from "@material-ui/core";
 
-const useStyles = makeStyles({
-  heading: {
-    position: "absolute",
-    top: "18rem",
-    fontStyle: "italic",
-    fontWeight: "500",
-    fontSize: "4.4rem",
-    lineHeight: "127px",
-    letterSpacing: " -0.001em",
-    background:
-      "linear-gradient(10.56deg,  #003145    0.49%,rgba(0, 152, 176, 0.26) 114.34%)",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
-  },
-
-  para: {
-    position: "absolute",
-    top: "51.6%",
-    fontStyle: "italic",
-    fontWeight: "500",
-    fontSize: "2.2rem",
-    lineHeight: "1rem",
-    letterSpacing: "-0.001em",
-    color: "#1488B5",
-  },
-  image: {
-    width: "32rem",
-    marginLeft: "6rem",
-  },
-  button: {
-    position: "absolute",
-    top: "60%",
-    background:
-      "linear-gradient(90.56deg, #0079A9 0.49%, rgba(100, 200, 240, 0.74) 114.34%)",
-    borderRadius: "28rem",
-    width: "16rem",
-    height: "4.2rem",
-
-    fontStyle: "italic",
-    fontWeight: "bold",
-    fontSize: "1.7rem",
-    textAlign: "center",
-    letterSpacing: "-0.01em",
-    color: "#FFFFFF",
-    letterSpacing: "0.005em",
-    filter: "drop-shadow(12px 12px 10px rgba(0, 0, 0, 0.42))",
-  },
-});
+import { useStyles } from "./Custom Styles/homeScreen";
 
 const HomeScreen = () => {
   const classes = useStyles();
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={6}>
-        <Typography className={classes.heading} variant="h1" component="h2">
-          Welcome To Secret Vault
-        </Typography>
-        <Typography className={classes.para} variant="h1" component="h2">
-          Your secrets are safe with us
-        </Typography>
-        <Button className={classes.button}>Continue</Button>
+    <>
+      {/* top part of page */}
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Typography className={classes.heading} variant="h1" component="h2">
+            Welcome To Secret Vault
+          </Typography>
+          <Typography className={classes.para} variant="h1" component="h2">
+            Your secrets are safe with us
+          </Typography>
+          <Button className={classes.button}>Continue</Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <img
+            src="assets/images/globe.png"
+            className={classes.image}
+            alt="globe"
+          />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={6}>
-        <img
-          src="assets/images/globe.png"
-          className={classes.image}
-          alt="globe"
-        />
+
+      {/* all secrets sections */}
+      <Typography className={classes.allSecrets} variant="h2" component="h4">
+        All Secrets
+      </Typography>
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={4}>
+          <img
+            className={classes.doc}
+            src="assets/images/folder.png"
+            alt="details_folder"
+          />
+          <Typography
+            className={classes.folder_name}
+            variant="h3"
+            component="h4"
+          >
+            Details
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <img
+            className={classes.doc}
+            src="assets/images/folder.png"
+            alt="details_folder"
+          />
+          <Typography
+            className={classes.folder_name}
+            variant="h3"
+            component="h4"
+          >
+            Documents
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <img
+            className={classes.doc}
+            src="assets/images/folder.png"
+            alt="details_folder"
+          />
+          <Typography
+            className={classes.folder_name}
+            variant="h3"
+            component="h4"
+          >
+            Education
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <img
+            className={classes.doc}
+            src="assets/images/folder.png"
+            alt="details_folder"
+          />
+          <Typography
+            className={classes.folder_name}
+            variant="h3"
+            component="h4"
+          >
+            Career
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <img
+            className={classes.doc}
+            src="assets/images/folder.png"
+            alt="details_folder"
+          />
+          <Typography
+            className={classes.folder_name}
+            variant="h3"
+            component="h4"
+          >
+            Medical
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} md={4}>
+          <img
+            className={classes.doc}
+            src="assets/images/folder.png"
+            alt="details_folder"
+          />
+          <Typography
+            className={classes.folder_name}
+            variant="h3"
+            component="h4"
+          >
+            Finance
+          </Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </>
   );
 };
 
